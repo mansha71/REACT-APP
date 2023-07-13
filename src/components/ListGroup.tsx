1493f730db0160f02ea47d5d7281f054fb3eae3e
Manan Sharma <mansharm71@gmail.com>
2023-07-12 23:00:46 -0400

    mea

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,24 @@
+import { Fragment } from "react";
+
 function ListGroup() {
-    return <h1>List group</h1>
+  const items = ["New York,San Fram,Tokyo,London,pouds"];
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item Found</p>;
+      </>
+    );
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
